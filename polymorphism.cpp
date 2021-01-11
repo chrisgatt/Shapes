9c43c3a4123d819206918b940a1c5a8ef596bbb6
f1iha <christophe.gattardi@gmail.com>
2021-01-11 19:10:04 +0100

    Template for lab 3

---
+++ polymorphism.cpp
@@ -0,0 +1,21 @@
+
+// //////////////////////////////////////////////////////////////////////
+// Import section
+// //////////////////////////////////////////////////////////////////////
+// STL
+#include<iostream>
+//Objects
+#include<Rectangle.hpp>
+#include<Square.hpp>
+
+int myType (const Rectangle &rect)
+{
+    // add your code here
+}
+
+int main ()
+{
+    Rectangle rect(10, 5);
+    Square sq(5);
+    return 0;
+}
