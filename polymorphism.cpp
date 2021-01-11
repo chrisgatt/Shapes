26b9af4a834a3744dae2aab8323f2f1d0227bd05
f1iha <christophe.gattardi@gmail.com>
2021-01-11 19:18:56 +0100

    Sol exo 3.1

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -11,11 +11,16 @@
 int myType (const Rectangle &rect)
 {
     // add your code here
+    std::cout << "Je suis un rectangle" << std::endl;
+    return 1;
 }
 
 int main ()
 {
     Rectangle rect(10, 5);
     Square sq(5);
+    // add your code here
+    std::cout << myType(rect) << std::endl;
+    std::cout << myType(sq) << std::endl;
     return 0;
 }
