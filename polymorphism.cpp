bcbae29e07025d567ea790b5322fa86abad68b0d
f1iha <christophe.gattardi@gmail.com>
2021-01-13 15:51:29 +0100

    Add circle

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -7,6 +7,7 @@
 //Objects
 #include<Rectangle.hpp>
 #include<Square.hpp>
+#include<Circle.hpp>
 
 int myType (const Rectangle &rect)
 {
@@ -15,12 +16,21 @@ int myType (const Rectangle &rect)
     return 1;
 }
 
+int myType (const Circle &circ)
+{
+    // add your code here
+    std::cout << "Je suis un cercle" << std::endl;
+    return 3;
+}
+
 int main ()
 {
     Rectangle rect(10, 5);
     Square sq(5);
+    Circle circ(5);
     // add your code here
     std::cout << myType(rect) << std::endl;
     std::cout << myType(sq) << std::endl;
+    std::cout << myType(circ) << std::endl;
     return 0;
 }
